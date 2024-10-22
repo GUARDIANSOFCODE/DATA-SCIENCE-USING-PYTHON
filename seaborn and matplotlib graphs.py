9ed69f8a28f81b254f609c84f185a1bb4153cfea
Newby19 <160457369+Newby19@users.noreply.github.com>
2024-10-22 18:16:23 +0530

    Update seaborn and matplotlib graphs.py

--- seaborn and matplotlib graphs.py
+++ seaborn and matplotlib graphs.py
@@ -1,10 +1,26 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
+
+# Load the Iris dataset
 data = sns.load_dataset("iris")
-sns.histplot(data["sepal_length"],bins=20)
-sns.kdeplot(data["sepal_length"],shade=True)
+
+# Histogram of Sepal Length
+sns.histplot(data["sepal_length"], bins=20)
+
+# KDE Plot with Shaded Area
+sns.kdeplot(data["sepal_length"], shade=True)
+
+# Rug Plot
 sns.rugplot(data["sepal_length"])
+
+# Box Plot of Sepal Length
 sns.boxplot(x=data["sepal_length"])
-sns.violinplot(x=data["species"],y=data["sepal_length"])
+
+# Violin Plot of Sepal Length by Species
+sns.violinplot(x=data["species"], y=data["sepal_length"])
+
+# ECDF Plot
 sns.ecdfplot(data["sepal_length"])
+
+# Show all plots
 plt.show()
