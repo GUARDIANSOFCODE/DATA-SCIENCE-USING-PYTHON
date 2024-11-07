344261209de33446c198809b41be684d11f7da53
Sankalp <160457369+GUARDIANSOFCODE@users.noreply.github.com>
2024-11-07 17:28:36 +0530

    Create BAR PLOT to visualize monthly sales.py

---
+++ BAR PLOT to visualize monthly sales.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+Months=["January","February","March","April","May"]
+Sales=[300,350,200,400,450]
+
+plt.bar(Months,Sales,color="green",label="MonthlySales")
+plt.xlabel("Months")
+plt.ylabel("Sales")
+plt.legend()
+plt.show()
