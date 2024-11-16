f14f27279139e3223b79ee152c7d125cec698be7
Sankalp <160457369+GUARDIANSOFCODE@users.noreply.github.com>
2024-11-16 16:11:48 +0530

    Create Simple line plot.py

---
+++ Simple line plot.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+x = [1, 2, 3, 4]
+y = [10, 20, 25, 30]
+
+# Simple line plot
+plt.plot(x, y)
+
+# Add title and labels
+plt.title('Simple Line Plot')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+
+# Show the plot
+plt.show()
